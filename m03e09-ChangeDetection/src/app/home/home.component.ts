28c30c2dc04dacd5c121d2348a80141a8b175698
Bill Huynh <youkaisanx@gmail.com>
2024-04-22 16:43:39 -0500

    Change Detection - OnPush

--- m03e09-ChangeDetection/src/app/home/home.component.ts
+++ m03e09-ChangeDetection/src/app/home/home.component.ts
@@ -1,9 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  OnInit,
+} from '@angular/core';
 
 @Component({
   standalone: true,
   selector: 'app-home',
   template: ` <p>{{ value }}</p> `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HomeComponent implements OnInit {
   value = 1;
