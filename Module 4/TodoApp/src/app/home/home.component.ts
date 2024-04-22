b78399fa79b2d0e9ae2f9812b395fff9f9b71e8f
Bill Huynh <youkaisanx@gmail.com>
2024-04-22 17:35:19 -0500

    Module 4 - todo app

---
+++ Module 4/TodoApp/src/app/home/home.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [],
+  template: `
+    <h2>todo</h2> 
+  `,
+  styles: ``
+})
+export default class HomeComponent {
+
+}
