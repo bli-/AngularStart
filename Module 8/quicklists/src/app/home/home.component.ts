c7a34b57c2a1078963e4f738b519921e19ca4d9a
Bill Huynh <youkaisanx@gmail.com>
2024-04-30 11:35:37 -0500

    Create foundation and service

--- Module 8/quicklists/src/app/home/home.component.ts
+++ Module 8/quicklists/src/app/home/home.component.ts
@@ -1,14 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, effect, inject, signal } from '@angular/core';
+import { ModalComponent } from '../shared/ui/modal.component';
+import { Checklist } from '../shared/interfaces/checklist';
+import { FormBuilder } from '@angular/forms';
+import { FormModalComponent } from '../shared/ui/form-modal.component';
+import { ChecklistService } from '../shared/data-access/checklist.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [ModalComponent, FormModalComponent],
   template: `
-    <p>
-      Hello world!
-    </p>
+    <header>
+      <h1>Quicklists</h1>
+      <button (click)="checklistBeingEdited.set({})">Add Checklist</button>
+    </header>
+
+    <app-modal [isOpen]="!!checklistBeingEdited()">
+      <ng-template>
+        <app-form-modal 
+          [title]="
+            checklistBeingEdited()?.title
+              ? checklistBeingEdited()!.title!
+              : 'Add Checklist'
+          "
+          [formGroup]="checklistForm"
+          (close)="checklistBeingEdited.set(null)"
+          (save)="checklistService.add$.next(checklistForm.getRawValue())"
+        />
+      </ng-template>
+    </app-modal>
   `,
   styles: ``
 })
-export default class HomeComponent {}
+export default class HomeComponent {
+  formBuilder = inject(FormBuilder)
+  checklistService = inject(ChecklistService);
+
+  checklistBeingEdited = signal<Partial<Checklist> | null>(null)
+
+  checklistForm = this.formBuilder.nonNullable.group({
+    title: ['']
+  });
+
+  constructor() {
+    effect(() => {
+      const checklist = this.checklistBeingEdited();
+
+      if (!checklist) {
+        this.checklistForm.reset();
+      }
+    })
+  }
+}
